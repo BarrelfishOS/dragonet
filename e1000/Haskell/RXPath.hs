a3dc6001392458122766d01311e916985e3784b7
pravin@inf.ethz.ch <pravin@inf.ethz.ch>
2012-06-20 03:03:47 +0200

    Added some more types, added comments, and cleared out the code.

--- e1000/Haskell/RXPath.hs
+++ e1000/Haskell/RXPath.hs
@@ -28,21 +28,23 @@ import qualified Data.Maybe
 type Packet = [Integer]
 type Hash = Integer
 type QueueID = Integer
+type Index = Int
+type CoreID = Integer
+type RedirectTbl = [(QueueID, CoreID)]
 
-
+-- TCP Hashing function
+-- Currently, it just adds all bytes to get the hash
 hashTCP :: Packet -> Hash
 hashTCP [] = 0
 hashTCP (x:xs) = x + hashTCP xs
 
+-- Select proper hash function based on the packet classification
+-- Currently only TCP is supported
+-- Need to add more
 selectHashFunction :: Packet -> Maybe (Packet -> Hash)
 selectHashFunction _ = Just hashTCP
 
--- This function will use ast 5 bits of the hash, and return value at that entry
-lookupRedirectionTable :: [(Integer, Integer)] -> Hash -> QueueID
-lookupRedirectionTable redirectionTable hash_value =
-        toInteger ( fst (redirectionTable!!fromIntegral hash_value))
-
-
+-- Get the packet hash by applying proper hashing function for given packet
 hashPacket :: Packet -> Hash
 hashPacket p =
     let hashFunction = selectHashFunction p in
@@ -50,11 +52,24 @@ hashPacket p =
         then 0
     else Data.Maybe.fromJust hashFunction p
 
+-- Given hash, convert into an index for Redirection Table
+-- This function will use last 5 bits of the hash
+hashToIndex :: Hash -> Index
+hashToIndex hash = fromIntegral (hash `mod` 127)
 
-classifyPacket :: Map.Map [Char] Bool -> [(Integer, Integer)] -> Packet
+-- For given lookup-table and hash, give the QueueID to which packet should go
+lookupRedirectionTable :: RedirectTbl -> Hash -> QueueID
+lookupRedirectionTable redirectionTable hash_value =
+        toInteger ( fst (redirectionTable!!(hashToIndex hash_value)))
+
+-- For given configuration, redirection-table and packet,
+--  find out the queueId which should get the packet
+classifyPacket :: Map.Map [Char] Bool -> RedirectTbl -> Packet
                     -> QueueID
 classifyPacket conf_map rdt p = lookupRedirectionTable rdt (hashPacket p)
 
+
+-- A test function to apply the algorithm on dummy packet
 handlePacket :: Integer
 handlePacket =
     let mm = Map.fromList([("enableChecksum", False),
@@ -64,13 +79,15 @@ handlePacket =
     -- FIXME: Sample values
         pkt = [1..64]
 
-    -- Table specifying which hash should go to which queue/core
+    -- Table specifying which hash should go to which (queue, core)
     -- FIXME: Sample values
         redirectionTable = replicate 127 (1, 1)
     in classifyPacket mm redirectionTable pkt
 
 main = print $ handlePacket
 
+
+-- ###################################################################
 -- pkt_status
 --      key value store
 --      for every decision and computation on packet, it stores the result
