8ff196318736705bd0b20c8fb1affc9e663b6ed4
pravin@inf.ethz.ch <pravin@inf.ethz.ch>
2012-06-28 18:48:17 +0200

    Started with L3 layer

--- e1000/Haskell/RXPath.hs
+++ e1000/Haskell/RXPath.hs
@@ -21,6 +21,7 @@ import qualified Data.Maybe
 
 -- Ethernet address
 data EtherAddr = EtherAddr BS.ByteString deriving (Show, Eq)
+data IPAddr = IPAddr BS.ByteString deriving (Show, Eq)
 
 -- Contents of packet
 data PacketData = PacketData {
@@ -32,6 +33,7 @@ data UnknownPacket = UnknownPacket {
     packetData :: PacketData
     } deriving (Show, Eq)
 
+-- #################### L2 packet ####################
 -- Ethernet packet
 data EthernetPacket = EthernetPacket {
     srcAddr :: EtherAddr
@@ -52,6 +54,16 @@ data L2Packet = BroadcastPkt EthernetPacket
                     }
     deriving (Show, Eq)
 
+-- #################### L3 packet ####################
+-- IPv4Packet
+data IPv4Packet = IPv4Packet {
+    srcIP :: IPAddr
+    , dstIP :: IPAddr
+    , protocol :: W.Word8
+    , headerChecksum :: [W.Word8]
+    , originalL2 :: L2Packet
+    } deriving (Show, Eq)
+
 -- L3 packet type
 data L3Packet = IPv4Pkt L2Packet
               | IPv6Pkt L2Packet
@@ -69,6 +81,15 @@ data L3Packet = IPv4Pkt L2Packet
 subList :: Int -> Int -> PacketData -> BS.ByteString
 subList start end payload =
    BS.take (end - start) $ BS.drop start $ bytes payload
+
+-- Convert [Word8] of size 2 into Word16
+convert2Word32 :: [W.Word8] -> W.Word32
+convert2Word32 (w1:w2:w3:w4:[]) =
+    (Bits.shiftL (fromIntegral w1 :: W.Word32) 24)
+    + (Bits.shiftL (fromIntegral w2 :: W.Word32) 16)
+    + (Bits.shiftL (fromIntegral w3 :: W.Word32) 8)
+    + (fromIntegral w4 :: W.Word32)
+convert2Word32 _ = error "incorrect length of array"
 
 
 -- Convert [Word8] of size 2 into Word16
@@ -167,7 +188,26 @@ validateL2Packet ethPkt
     where l2Pkt = toL2Packet ethPkt
 
 
+-- Get payload from L2 packet
+getL2Payload :: L2Packet -> PacketData
+getL2Payload (BroadcastPkt ethPkt) = payload ethPkt
+getL2Payload (UnicastPkt ethPkt) = payload ethPkt
+getL2Payload (MulticastPkt ethPkt) = payload ethPkt
+getL2Payload (InvalidPktL2 ethPkt _) =
+                    error "invalid L2 packet in L3 processing"
 
+{-
+-- Check if given protocol is IPv4
+isIPv4 :: PacketData -> Bool
+isIPv4 l2payload =
+    convert2Word32 $ subList 0 4 l2payload
+
+-- #################### L3 packet parsing ####################
+toL3Layer :: L2Packet -> L3Packet
+toL3Layer l2Pkt
+    | isIPv4 l2Payload
+    where l2Payload = getL2Payload l2Pkt
+-}
 -- #################### Packet generator ####################
 
 -- getNextPacket for processing:  Currently it is generated/hardcoded.
