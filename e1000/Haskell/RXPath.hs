c49bf523378980b20b23589abe8723da07205746
pravin@inf.ethz.ch <pravin@inf.ethz.ch>
2012-07-09 11:10:17 +0200

    Started working on the NICState type

--- e1000/Haskell/RXPath.hs
+++ e1000/Haskell/RXPath.hs
@@ -1,14 +1,15 @@
 {-
 
-Models the processing performed by e1000 RX path on incoming packet.
+Model for processing performed by e1000 RX path on incoming packet.
 
 Aim:
   --Input: Incoming packet
-  --        Configuration
+  --       Configuration
+  --
   --Output:
-  --    Packet in classified queue
-  --    Notification of packet generation
-  --    Status of packet in descriptor (TODO)
+  --    Classified packet
+  --    QueueID where packet will go
+  --    CoreID where notification will be sent
 -}
 
 module Main (main) where
@@ -19,7 +20,6 @@ import qualified Data.Word as W
 import qualified Data.Bits as Bits
 import qualified Data.Maybe
 
-
 -- Ethernet address
 data EtherAddr = EtherAddr BS.ByteString deriving (Show, Eq)
 data IPAddr = IPAddr BS.ByteString deriving (Show, Eq)
@@ -29,9 +29,6 @@ data PortNo = PortNo W.Word16 deriving (Show, Eq)
 data PacketData = PacketData {
         bytes :: BS.ByteString
         } deriving (Show, Eq)
-
--- type for hash of PacketData
-type Hash = Integer
 
 -- unclassified/unprocessed packet
 data UnknownPacket = UnknownPacket {
@@ -132,11 +129,16 @@ data L4Packet =  L4BasePkt L4BasePacket
 
 
 -- #################### Redirection table ####################
-type Index = Integer
-type QueueID = Integer
-type CoreID = Integer
+
+type QueueID = Integer  -- ID for the hardware queue
+type CoreID = Integer  -- ID for the CPU which can accept notification
+
+-- Redirection table holding the mapping between hash values and CPU cores
 type RedirectTbl = [(QueueID, CoreID)]
 
+-- Mechanism to convert hash into index for redirection Table.
+type Hash = Integer -- Hash of the packet
+type Index = Integer -- Index within Redirection Table
 
 -- #################### Ethernet packet parsing ####################
 
@@ -495,6 +497,8 @@ hashPacket (L4BasePkt l4Pkt) = hashL4Packet l4Pkt
 hashPacket (InvalidPktL4 _ _) = error "hashing invalid pkt"
 
 
+-- ##################### Dealing with redirection table ###########
+
 -- Given hash, convert into an index for Redirection Table
 -- This function will use last 5 bits of the hash
 hashToIndex :: Hash -> Index
@@ -538,88 +542,3 @@ main = print $ (show l4Pkt)
         core = lookupCoreInRedirectionTable redirectionTable hash
 
 -- #################### EOF ####################
-
-{-
-
-Is it a valid packet?
-Which queue it belongs?
-Which CPU will be interrupted?
-What is the state of the system?  (packet count, status, etc)
-
-Combined data type,
--}
-
-{-
-
-type Packet = [Integer]
-type Hash = Integer
-type Index = Int
-type QueueID = Integer
-type CoreID = Integer
-type RedirectTbl = [(QueueID, CoreID)]
-
--- TCP Hashing function
--- Currently, it just adds all bytes to get the hash
--- FIXME: Dummy hash function
-hashTCP :: Packet -> Hash
-hashTCP [] = 0
-hashTCP (x:xs) = x + hashTCP xs
-
--- Select proper hash function based on the packet classification
--- Currently only TCP is supported
--- Need to add more
-selectHashFunction :: Packet -> Maybe (Packet -> Hash)
-selectHashFunction _ = Just hashTCP
-
--- Get the packet hash by applying proper hashing function for given packet
-hashPacket :: Packet -> Hash
-hashPacket p =
-    let hashFunction = selectHashFunction p in
-    case hashFunction of
-        Nothing -> 0
-        Just fn -> fn p
-
--- Given hash, convert into an index for Redirection Table
--- This function will use last 5 bits of the hash
-hashToIndex :: Hash -> Index
-hashToIndex hash = fromIntegral (hash `mod` 127)
-
--- Returns a default entry in redirection table
-lookupDefaultEntry :: RedirectTbl -> QueueID
-lookupDefaultEntry redirectionTable =
-        toInteger ( fst (redirectionTable!!(hashToIndex 0)))
-
--- For given lookup-table and hash, give the QueueID to which packet should go
-lookupRedirectionTable :: RedirectTbl -> Hash -> QueueID
-lookupRedirectionTable redirectionTable hash_value =
-        toInteger ( fst (redirectionTable!!(hashToIndex hash_value)))
-
--- For given configuration, redirection-table and packet,
---  find out the queueId which should get the packet
-classifyPacket :: Map.Map [Char] Bool -> RedirectTbl -> Packet -> QueueID
-classifyPacket conf_map rdt p =
-    let conf = Map.lookup "enableMultiQueue" conf_map in
-    case conf of
-        Nothing -> lookupDefaultEntry rdt
-        Just a -> lookupRedirectionTable rdt (hashPacket p)
-
--- A test function to apply the algorithm on dummy packet
-handlePacket :: Integer
-handlePacket =
-    let mm = Map.fromList([("enableChecksum", False),
-                        ("enableMultiQueue", True)])
-
-    -- The packet holding bytes
-    -- FIXME: Sample values
-        pkt = [1..64]
-
-    -- Table specifying which hash should go to which (queue, core)
-    -- FIXME: Sample values
-        redirectionTable = replicate 127 (1, 1)
-    in classifyPacket mm redirectionTable pkt
-
-main = print $ handlePacket
-
--}
-
-
