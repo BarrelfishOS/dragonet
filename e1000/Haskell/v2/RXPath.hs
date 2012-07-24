2a4fd3042f37ef6561023a52e2f35cc81c1b1ec4
pravin@inf.ethz.ch <pravin@inf.ethz.ch>
2012-07-24 11:58:52 +0200

    Added some selector functions

--- e1000/Haskell/v2/RXPath.hs
+++ e1000/Haskell/v2/RXPath.hs
@@ -13,6 +13,10 @@ data Packet = RawPacket {
             | L3Packet Packet
             | TCPPacket Packet
             | UDPPacket Packet
+            | InvalidPacket {
+                contents :: Packet
+                , reason :: String
+             }
             deriving (Show, Eq)
 
 type QueueID = Integer  -- ID for the hardware queue
@@ -41,6 +45,50 @@ decide classifier actionList pkt =
             where
                 nextAction = actionList !! (fromIntegral $ classifier pkt)
 
+-- #################### Few classifiers ####################
+
+-- Validate L2 packet
+isValidL2 :: Packet -> Integer
+isValidL2 (RawPacket pktContents) = 1 -- FIXME: Assuming valid packet
+isValidL2 _ = error "invalid type packet passed to isValidL2"
+
+-- Check if it is valid IPv4 packet
+isValidIPv4 :: Packet -> Integer
+isValidIPv4 (L2Packet pkt) = 1 -- FIXME: Assuming valid packet
+isValidIPv4 _ = error "invalid type packet passed to isValidv4"
+
+-- Check if it is valid IPv6 packet
+isValidIPv6 :: Packet -> Integer
+isValidIPv6 (L2Packet pkt) = 1 -- FIXME: Assuming valid packet
+isValidIPv6 _ = error "invalid type packet passed to isValidv6"
+
+-- Check if it is valid L3 packet
+isValidL3 :: Packet -> Integer
+isValidL3 (IPv4Packet pkt) = 1 -- FIXME: Assuming valid packet
+isValidL3 (IPv6Packet pkt) = 1 -- FIXME: Assuming valid packet
+isValidL3 _ = error "invalid type packet passed to isValidL3"
+
+-- Check if it is valid TCP packet
+isValidTCP :: Packet -> Integer
+isValidTCP (L3Packet pkt) = 1 -- FIXME: Assuming valid packet
+isValidTCP _ = error "invalid type packet passed to isValidTCP"
+
+-- Check if it is valid UDP packet
+isValidUDP :: Packet -> Integer
+isValidUDP (L3Packet pkt) = 1 -- FIXME: Assuming valid packet
+isValidUDP _ = error "invalid type packet passed to isValidUDP"
+
+-- Selects queue after applying filters based on packet type
+selectQueue :: Packet -> Integer
+selectQueue (TCPPacket pkt) = 1 -- FIXME: get hash and select queue
+selectQueue (UDPPacket pkt) = 1 -- FIXME: get hash and select queue
+selectQueue _ = 0 -- Default queue (when no other filter matches)
+
+
+selectProperQueue :: Packet -> Action
+selectProperQueue pkt = decide selectQueue acList pkt
+                    where
+                        acList = [(InQueue 0), (InQueue 1)]
 
 -- #################### Main module ####################
 -- main function
