4594f122df511ea27d48d48a1a02fcf0b6514bf4
pravin@inf.ethz.ch <pravin@inf.ethz.ch>
2013-05-23 17:37:23 +0200

    Added basic code for Broadcom chip BCM5704.

---
+++ PRG/BCM5704.hs
@@ -0,0 +1,70 @@
+
+module Main (main) where
+
+import qualified NICState as NS
+import qualified DecisionTree as DT
+import qualified ConvertDecision as CD
+
+
+-- Config
+-- eBCMNumQueues = 16 -- Actual number
+
+-- Using 4 to reduce the complexity
+eBCMNumQueues = 4
+
+
+-- Receive queue
+qname s i = s ++ (show i)
+
+eBCMRxInterrupt next = [DT.Decision {
+        DT.compute = (DT.Computation "RxInterrupt"),
+        DT.possibleActions = next
+}]
+
+eBCMInQueue i = d
+    where
+        [d] = [(DT.InQueue i)]
+
+makeQueues n = map eBCMInQueue [1..n]
+eBCMInQueues = makeQueues eBCMNumQueues
+
+
+eBCMFlexibleFilter next = [DT.Decision {
+        DT.compute = (DT.Computation "FlexibleFilter"),
+        DT.possibleActions = [DT.Dropped] ++ eBCMInQueues ++ next
+}]
+
+eBCMRxChecksumming next = [DT.Decision {
+        DT.compute = (DT.Computation "RxChecksumOffload"),
+        DT.possibleActions = next
+}]
+
+eBCMQueueSelect =
+        eBCMFlexibleFilter []
+
+
+eBCMVLANTagging next = [DT.Decision {
+        DT.compute = (DT.Computation "VLANtagging"),
+        DT.possibleActions = next
+}]
+
+eBCML2Filter next = eBCMVLANTagging $ next
+
+
+-- Very basic overview of RX path (see figure 7.1)
+eBCMRxPath = d
+    where
+        [d] = eBCML2Filter $ eBCMRxChecksumming $ eBCMQueueSelect
+
+packetHandlingDes :: DT.Decision
+packetHandlingDes = eBCMRxPath
+
+-- #################### Main module ####################
+
+-- main function
+main = do
+        putStrLn out4
+    where
+        myTree = CD.convertDT packetHandlingDes
+        out4 = CD.printAbstractTree myTree
+
