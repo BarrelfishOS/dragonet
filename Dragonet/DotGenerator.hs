5bd5dd38363676fc115d5783df15960a35f96e0f
Antoine Kaufmann <toni@famkaufmann.info>
2013-06-29 22:33:12 +0200

    Added ports and double-edges to the dot output

--- Dragonet/DotGenerator.hs
+++ Dragonet/DotGenerator.hs
@@ -17,28 +17,61 @@ import Data.Maybe
 -- Get string label for GNode
 gLabelStr gn = NB.graphLabelStr (OP.gLabel gn)
 
-dotNode name label shape =
-    "    " ++ name ++ "[label=\"" ++ label ++ "\",shape=" ++ shape ++ "];\n"
-dotEdge from to =
-    "    " ++ from ++ " -> " ++ to ++ ";\n"
+dotNode name label style nPorts =
+    "    " ++ name ++ "[label=\"" ++ l ++ "\",shape=record" ++ s ++ "];\n"
+    where
+        s = if style /= "" then ",style=" ++ style else ""
+        port i = "|<p" ++ (show i) ++ "> " ++ (show (i - 1)) ++ " "
+        ports =
+            case (concat (map port [1..nPorts])) of
+                h:t -> t
+                _ -> ""
+        l = "{" ++ label ++ "|{" ++ ports ++ "}}"
+
+dotEdge (from,port) to =
+    "    " ++ f ++ " -> " ++ to ++ ";\n"
+    where
+        f = from ++ ":p" ++ (show port)
+dotDoubleEdge (from,port) to =
+    "    " ++ from ++ " -> " ++ to ++ "[color=\"black:black\"];\n"
+
+-- Number of ports required for node
+nPorts (OP.BinaryNode _) = 2
+nPorts (OP.NaryNode l) = length l
 
 -- Get dot definition for specified node
 nodeDefinition :: (OP.Node, String) -> String
-nodeDefinition ((OP.Des (OP.Decision gn)), nn) =
-    dotNode nn (gLabelStr gn) "box"
-nodeDefinition ((OP.Opr (OP.Operator gn)), nn) =
-    dotNode nn (gLabelStr gn) "oval"
+nodeDefinition (n, nn) =
+    case n of
+        (OP.Des (OP.Decision gn)) ->
+            dotNode nn (gLabelStr gn) "" (nPorts (OP.getNodeEdges n))
+        (OP.Opr (OP.Operator gn)) ->
+            dotNode nn (gLabelStr gn) "rounded" (nPorts (OP.getNodeEdges n))
 
 -- Get dot definition for edges starting at the specified node
 edgeDefinition :: [(OP.Node, String)] -> (OP.Node, String) -> String
 edgeDefinition dict (n, nn) =
-    concat (map edgeTo endpoints)
+     case OP.getNodeEdges n of
+        (OP.NaryNode es) ->
+            edgesToEps dotEdge (prefixLL (concat es))
+        (OP.BinaryNode (ts,fs)) ->
+            (edgesToEps dotDoubleEdge (doubleEps ts fs)) ++
+            (edgesToEps dotEdge (singleEps ts fs))
     where
-        edgeTo n = dotEdge nn (fromJust (lookup n dict))
-        endpoints =
-            case OP.getNodeEdges n of
-                (OP.BinaryNode (ts,fs)) -> ts ++ fs
-                (OP.NaryNode es) -> concat es
+        prefixL (l,p) = zip l (replicate (length l) p)
+        prefixLL l = zip l [1..(length l)]
+        -- endpoints in both lists
+        doubleEps ts fs = prefixL ((L.intersect ts fs),0)
+        -- endpoints in only one list
+        singleEps ts fs = (prefixL (nfs, 1)) ++ (prefixL (nts, 2))
+            where
+                dbl = L.intersect ts fs
+                nts = (L.nub ts) L.\\ dbl
+                nfs = (L.nub fs) L.\\ dbl
+            
+
+        edgesToEps f eps = concat (map (edgeTo f) eps)
+        edgeTo f (n,p) = f (nn,p) (fromJust (lookup n dict))
 
 -- Get associative list between node and their dot names, assumes each node
 -- only occurs once in the list
