e845e27d8868bd9c839a6915139cd4ef78f6c45e
Antoine Kaufmann <antoinek@student.ethz.ch>
2013-07-26 19:07:57 +0200

    Remove old embedding algorithm parts

--- Dragonet/dsltest/Embedding.hs
+++ Dragonet/dsltest/Embedding.hs
@@ -23,38 +23,6 @@ import qualified Debug.Trace as TR
 import Operations
 
 ----------------------------------------------------------------
--- To support conversion from Node to String and vice versa
-
-convertEdgeP :: (Node, String, Node) -> (String, String, String)
-convertEdgeP (n1, p, n2) = ((nodeDefinition n1), p, (nodeDefinition n2))
-
-
-
-convertLabelToNode :: [Node] -> String -> Node
-convertLabelToNode nlist label = L.head $
-    filter (\ x -> (nodeDefinition x) == label) nlist
-
-
-convertEdgeToNodeP :: [Node] -> (String, String, String) -> (Node, String, Node)
-convertEdgeToNodeP nlist (a, p, b) = (a', p, b')
-    where
-    a' = convertLabelToNode nlist a
-    b' = convertLabelToNode nlist b
-
-
-
--- Get string label for GNode
-gLabelStr gn = (gLabel gn)
-
-nodeDefinition :: Node -> String
-nodeDefinition n =
-    case n of
-        (Des (Decision gn))           -> ((gLabelStr gn) )
-        (Opr (Operator gn))           -> ((gLabelStr gn) )
-        (Conf (Configuration gn))     -> ((gLabelStr gn) )
-
-
-----------------------------------------------------------------
 --  Node datatype manipulation
 ----------------------------------------------------------------
 
@@ -78,34 +46,10 @@ getOutEdgesP root = nextLevel ++ deeperLevels
 getDepEdgesP :: Node -> [(Node,String,Node)]
 getDepEdgesP root = L.nub $ getOutEdgesP root
 
-getSoftStartNode :: Node
-getSoftStartNode = getDecNode "inSoftware" "" (NaryNode []) []
-
 
 ----------------------------------------------------------------
 --      Generic list based functionalities
 ----------------------------------------------------------------
-
--- Topological sort on edge list representation
-topSort :: Eq a => [(a,a)] -> [a]
-topSort [] = []
-topSort es =
-    noIncoming ++ orphaned ++ (topSort newEdges)
-    where
-        -- Is n a successor of another Node?
-        notSucc n = MB.isNothing $ L.find (\x -> (snd x) == n) es
-        -- All nodes without incoming edges
-        noIncoming = filter (notSucc) $ L.nub $ map fst es
-        -- edges that don't start at noIncoming nodes
-        newEdges = filter (\x -> notElem (fst x) noIncoming) es
-        -- edges that start at noIncoming nodes
-        dropped = filter (\x -> elem (fst x) noIncoming) es
-        -- Sink nodes (without outgoing edges) that lost their incoming edges
-        -- those also vanish from the edges list
-        isOrphaned n = MB.isNothing $ L.find
-                (\x -> ((snd x) == n) || ((fst x) == n)) newEdges
-        orphaned = L.nub $ filter isOrphaned $ map snd dropped
-
 
 -- Topological sort on edge list representation
 topSort3 :: Eq a => [(a,b,a)] -> [a]
@@ -135,40 +79,11 @@ topSort3 es =
 -- Support functions which work on edges with labels
 ----------------------------------------------------------------
 
-getfst :: (a, b, c) -> a
-getfst (x, _, _) = x
-
 get3snd :: (a, b, c) -> b
 get3snd (_, p, _) = p
 
 getThird :: (a, b, c) -> c
 getThird (_, _, z) = z
-
-
--- For given edges get all nodes which are at source
-getSRCNodesP :: (Eq a) => [(a, b, a)] -> [a]
-getSRCNodesP gedges = L.map (getfst) gedges
-
--- For given edges get all nodes which are at destination
-getDESTNodesP :: (Eq a) => [(a, b, a)] -> [a]
-getDESTNodesP gedges = L.map (getThird) gedges
-
--- For given edges get all nodes which appear anywhere
-getALLNodesP :: (Eq a) => [(a, b, a)] -> [a]
-getALLNodesP gedges = L.nub $ (srcNodes ++ destNodes)
-    where
-    srcNodes = getSRCNodesP gedges
-    destNodes = getDESTNodesP gedges
-
--- Find all edges where given Node appear at source
-locateAsSRCEdgeP :: (Eq a) => [(a, b, a)] -> a -> [(a, b, a)]
-locateAsSRCEdgeP gedges gn = L.filter (\ (x, _, _) -> gn == x ) gedges
-
--- Find all edges where given Node appear at destination (sink)
-locateAsDESTEdgeP :: (Eq a) => [(a, b, a)] -> a -> [(a, b, a)]
-locateAsDESTEdgeP gedges gn = L.filter (\ (_, _, z) -> gn == z ) gedges
-
-
 
 
 ----------------------------------------------------------------
@@ -180,9 +95,6 @@ locateAsDESTEdgeP gedges gn = L.filter (\ (_, _, z) -> gn == z ) gedges
 edgeStart :: (a,b,c) -> a
 edgeStart (n,_,_) = n
 
-edgeLabel :: (a,b,c) -> b
-edgeLabel (_,l,_) = l
-
 edgeEnd :: (a,b,c) -> c
 edgeEnd (_,_,n) = n
 
@@ -190,82 +102,11 @@ nodesSet :: ((Ord a),(Ord b)) => S.Set (a,b,a) -> S.Set a
 nodesSet s = (S.map edgeStart s) `S.union` (S.map edgeEnd s)
 
 inEdges n s = S.filter ((n ==) . edgeEnd) s
-outEdges n s = S.filter ((n ==) . edgeStart) s
 inNeighbours n s = S.map edgeStart $ inEdges n s
 
 edgeNMap :: (a -> c) -> (a, b, a) -> (c, b, c)
 edgeNMap f (a,b,c) = (f a, b, f c)
 
-
---embeddingV3Step sP sE [] = sE
---embeddingV3Step :: ((Show a), (Ord a)) => S.Set (a,a) -> S.Set (a,a) -> S.Set (a,a) -> S.Set (a,a)
-embeddingV3Step :: ((Ord a), (Ord b)) => S.Set (a,b,a) -> (S.Set (a,b,a), S.Set a) -> (S.Set (a,b,a), S.Set a) -> S.Set (a,b,a)
-embeddingV3Step sP (sE,sEv) (sU,sUv)
-    | S.null sU = sE
-    | sEv == sEv' = error "E not changing"
-    | otherwise = embeddingV3Step sP (sE',sEv') (sU',sUv')
-    where
-        sPv = nodesSet sP
-
-        -- Find dependencies of specified node in LPG and PRG
-        deps n = (inNeighbours n sE) `S.union` (inNeighbours n sP)
-
-        -- Find a node whose dependencies are already embedded
-        v = S.findMin $ S.filter (flip S.isSubsetOf sEv . deps) sUv
-
-        -- v's dependencies from PRG
-        sEprg = S.filter ((v ==) . edgeEnd) sP -- S.map (mkEdge v) $ outNeighbours v sP
-        -- v's dependencies from LPG
-        sElpg' = S.filter (flip S.notMember sPv . edgeStart) $ inEdges v sU
-        sElpg = if v `S.notMember` sPv then sElpg' else S.empty
-
-        sE' = sE `S.union` sEprg `S.union` sElpg
-        -- remove edges starting from v in
-        sU' = sU S.\\ (inEdges v sU)
-
-        -- Move vertex from E to U
-        sEv' = S.insert v sEv
-        sUv' = S.delete v sUv
-
-
--- Takes Nodes, converts them into String, applies the embedding algorithm
---      convert back the results into Node format
-embeddingV3Wrapper ::  Node -> Node -> [(Node, String, Node)]
-embeddingV3Wrapper prg lpg =
-    result ++ softImplEdge
-    where
---    result = convert $ embeddingV3Step prgEdges (S.empty,S.empty) (lpgEdges, allNodesSet)
---    result = convert prgHWedges'
-    result = convert $ embeddingV3Step prgHWedges' (S.empty,S.empty) (lpgEdges, allNodesSet)
-
---    allNodesSet = (nodesSet lpgEdges) `S.union` (nodesSet prgEdges)
-    allNodesSet = (nodesSet lpgEdges) `S.union` (nodesSet prgHWedges')
-    prgNodes = L.nub $ nTreeNodes prg
-
-    prgEmulatedNodes = L.map (nodeDefinition) $
-        L.filter (not . isNodeEmbebible) prgNodes
-
-
-    nodeList = L.nub (nTreeNodes lpg  ++ prgNodes ++ [swStartNode])
-    convert n = map (\ x -> convertEdgeToNodeP nodeList x) $ S.toList n
-    swStartNode = getSoftStartNode
-
-    lpgEdges = S.fromList $ L.map convertEdgeP $ removeDroppedNodesP
-            $  getDepEdgesP lpg
---    prgEdges = S.fromList $ L.map convertEdgeP $ removeDroppedNodesP
---            $ getDepEdgesP prg
-    prgEdges = L.map convertEdgeP $ removeDroppedNodesP $ getDepEdgesP prg
-
-    prgHWedges' = S.fromList $ prgHWedges
-
---    prgHWedges = prgEdges
-
-    (prgHWedges, prgSWnodes) =  removeSWnodePRGP (L.nub prgEdges)
-                                (isPRGNodeEmulated prgEmulatedNodes)
-
-    defaultQueue = getDecNode "toDefaultQueue" ""
-        (BinaryNode ([], [])) []
-    softImplEdge =  [((defaultQueue), "", (swStartNode))]
 
 
 
@@ -307,9 +148,6 @@ isLPG :: ENode -> Bool
 isLPG (ELPG _ _ _) = True
 isLPG _ = False
 
-isPRG :: ENode -> Bool
-isPRG = not . isLPG
-
 isSoftware :: ENode -> Bool
 isSoftware (ELPG _ _ _) = True
 isSoftware (EPRG _ a _) = elem "software" a
@@ -345,18 +183,15 @@ embeddingV4 prg lpg =
     memberL n s = not $ S.null $ S.filter (matchLabel n) s
     isSubsetOfL a b = all (flip memberL b) $ S.toList a
     nodeL n s = S.findMin $ S.filter (matchLabel n) s
-    setMinusL a b = S.filter (not . flip memberL b) a
 
     prgDeps n = inNeighbours n prg
     lpgDeps n = inNeighbours n lpg
 
     embeddingV4Step (sEn,sEe) sUn
         | S.null sUn = sEe
-        {-| not $ sEn `S.isProperSubsetOf` sEn' = error "E not growing"-}
         | otherwise = embeddingV4Step (sEn',sEe') sUn'
         where
             (sEn',sEe') = (sEn `S.union` newNodes, sEe `S.union` newEdges)
-            --sUn' = sUn `setMinusL` newNodes
             sUn' = S.delete v sUn
 
             -- Find a node whose dependencies are already embedded
@@ -401,8 +236,7 @@ serializeV4 graph =
         queueEdges = S.map queueEdge $ S.filter isQueue $ nodes
         queueEdge  q = (q,"out",boundaryNode $ enLabel q)
         fixedEdges = S.fromList $ concatMap fixEdge $ S.toList $ crossingEdges
-        --fixEdge (s,p,e) = [(s,p,boundaryNode), (boundaryNode,boundaryNodePort,e)]
-        fixEdge (s,p,e) = [(boundaryNode $ enTag e,boundaryNodePort,e)]
+        fixEdge (_,_,e) = [(boundaryNode $ enTag e,boundaryNodePort,e)]
         boundaryNode l = ELPG "SoftwareEntry" [] l
         boundaryNodePort = "out"
 
@@ -436,85 +270,6 @@ convertV4 edges =
                 edge p = (p, map (convertNode . getThird) $ S.toList $ S.filter ((== p) . get3snd) outedges)
                 epl = map edge $ S.toList ports
                 e = NaryNode epl
-
-getConstraints :: S.Set (ENode,String,ENode) -> M.Map ENode (S.Set (ENode,String))
-getConstraints g =
-    gcStep nodes M.empty
-    where
-        nodes = (S.map edgeStart g) `S.union` (S.map edgeEnd g)
-        gcStep ns a
-            | S.null ns = a
-            | otherwise = gcStep ns' a'
-            where
-                directConstraints = S.map (\(a,b,_) -> (a,b)) $ inEdges n g
-                indirectConstraints = S.foldr S.union S.empty $ S.map (\x -> MB.fromJust $ M.lookup x a) $ inNeighbours n g
-                constraints = directConstraints `S.union` indirectConstraints
-                a' = M.insert n constraints a
-
-                ns' = n `S.delete` ns
-                n = S.findMin $ S.filter isSatisfied ns
-                isSatisfied m = S.null $ inNeighbours m g `S.intersection` ns
-
-data Ternary = TTrue | TFalse | TZ
-    deriving (Show, Eq, Ord)
-
-tcombine TTrue TTrue = TTrue
-tcombine TTrue TZ = TTrue
-tcombine TZ TTrue = TTrue
-tcombine TFalse TFalse = TFalse
-tcombine TZ TFalse = TFalse
-tcombine TFalse TZ = TFalse
-tcombine TZ TZ = TZ
-tcombine TTrue TFalse = error "Contradiction"
-tcombine TFalse TTrue = error "Contradiction"
-
-tLabel TTrue = "T"
-tLabel TFalse = "F"
-
-fixedValue :: ENode -> S.Set (ENode,String) -> Ternary
-fixedValue n c
-    | isHardware n = TZ
-    | (prefix `L.isPrefixOf` lbl) = check
-    | otherwise = TZ
-    where
-        lbl = enLabel n
-        prefix = "IsUDPDest"
-        check = S.foldl tcombine TZ $ S.map val $ S.filter (L.isPrefixOf ("HW" ++ prefix) . enLabel . fst) c
-        val (en,l)  =
-            if el == "HW" ++ lbl then
-                if l == "T" then
-                    TTrue
-                else if l == "F" then
-                    TFalse
-                else
-                    TZ
-            else if l == "T" then
-                TFalse
-            else
-                TZ
-            where
-                el = enLabel en
-
-applyConstraints :: S.Set (ENode,String,ENode) -> S.Set (ENode,String,ENode)
-applyConstraints g =
-    S.foldl step g nodes
-    where
-        nodes :: S.Set ENode
-        nodes = (S.map edgeStart g) `S.union` (S.map edgeEnd g)
-        constraints = getConstraints g
-        step :: S.Set (ENode,String,ENode) -> ENode -> S.Set (ENode,String,ENode)
-        step g' n
-            | fv == TZ = g'
-            | otherwise = TR.trace("fixed node " ++ (show n) ++ " to " ++ (show fv)) (g'')
-            where
-                c = MB.fromJust $ M.lookup n constraints
-                fv = fixedValue n c
-                l = tLabel fv
-                g'' = (g' S.\\ dropEdges) --`S.union` addEdges
-                dropEdges = S.filter (\e -> (edgeStart e == n) && (edgeLabel e /= l)) g'
-                --addEdges = S.foldl S.union S.empty $ S.map edge $ inEdges n g'
-                edge (a,b,_) = S.map (\e -> (a,b,e)) $ S.map edgeEnd $ S.filter ((== l) . edgeLabel) $ outEdges n g'
-
 
 -- Track recursively backwards along incoming edges to specified node and
 -- collect edges
@@ -556,9 +311,6 @@ embeddingV4Wrapper prg lpg =
     -- Tag software node with queue label
     tagLPG q n
         | isHardware n = n
-        {-| enIsAnd n = f ("AND:" ++ (q ++ (drop 4 l))) a q
-        | enIsOr n = f ("OR:" ++ (q ++ (drop 3 l))) a q
-        | otherwise = f (q ++ l) a q-}
         | enIsAnd n = f l a q
         | enIsOr n = f l a q
         | otherwise = f l a q
@@ -575,7 +327,6 @@ embeddingV4Wrapper prg lpg =
     embedded = foldl S.union S.empty $ map embeddedSWQ $ queues
 
     serialized = serializeV4 embedded
-    constrained = applyConstraints serialized
 
 convertEdgeV4 :: (String -> [String] -> String -> ENode) -> (Node, String, Node) -> (ENode, String, ENode)
 convertEdgeV4 f (n1, p, n2) =
@@ -583,75 +334,6 @@ convertEdgeV4 f (n1, p, n2) =
     where
         node n = f (nLabel n) (nAttributes n) ""
         
-----------------------------------------------------------------
---  A version that works on the String type
-----------------------------------------------------------------
-
-
-isNodeEmbebible :: Node -> Bool
-isNodeEmbebible n
-    | L.elem "inSoft" attrs = False
-    | otherwise               = True
-    where
-    attrs = getNodeAttributes n
-
-
-------------------------------------------------------------------------
--- Graph trimming helper functions for graphs with edge label
-------------------------------------------------------------------------
-
-{-
- - FIXME: I need to clearly understand, what exactly it means to drop the
- -      node from PRG.  Specially, in presence of True/False ports,
- -      it becomes much more complicated.
- -}
-removeNodeP :: (Eq a) => (Show a) => (Show b) => [(a, b, a)] -> a -> [(a, b, a)]
-removeNodeP edgesG v =
-    --TR.trace ("Request to remove " ++ show v ++ " from graph " ++ show edgesG)
-    edgesG''
-    where
-    -- Note: assumption: there is only one node in graph with given name
-
-    sEdges n = L.map (\ (x', l) -> (x', l, n)) $ L.map
-        (\ (x, y, _) -> (x, y)) $ locateAsDESTEdgeP edgesG v
-    edgesG' = L.concatMap (\ (x, l, y) -> if x == v then (sEdges y)
-                else [(x, l, y)]) edgesG
-
-    cNodes = getDESTNodesP $ locateAsSRCEdgeP edgesG v
-    dEdges n l = L.map (\ x' -> (n, l, x')) cNodes
-    edgesG'' = L.concatMap (\ (x, l, y) -> if y == v then (dEdges x l)
-                else [(x, l, y)]) edgesG'
-
-
---
--- remove all the nodes which are marked for sw execution
---      prgFn is function which tells if given node a is software node or not
-removeSWnodePRGP :: (Eq a) => (Show a) => (Show b) => [(a, b, a)] -> (a -> Bool)
-    -> ([(a, b, a)], [a])
-removeSWnodePRGP prgEdges prgFn = (prgEdges', trueV)
-    where
-
-    -- Find all the nodes
-    allV = getALLNodesP prgEdges
-
-    -- Find set of nodes which should go in SW
-    trueV = L.filter (prgFn) allV -- goes in software
-
-    -- Delete these nodes from PRG
-    prgEdges' =
-        L.foldl (\ acc x -> removeNodeP acc x) prgEdges trueV
-
-
-------------------------------------------------------------------
---
-------------------------------------------------------------------
-
-
-isPRGNodeEmulated :: (Eq a) => [a] -> a -> Bool
-isPRGNodeEmulated vList v = L.elem v vList
-
-
-
 ----------------------------------------------------------------
 --      Testing code
 ----------------------------------------------------------------
