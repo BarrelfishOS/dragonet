79133ed79b1498cf98607352567f9c3dfc2364c0
Pravin Shinde <pravin@inf.ethz.ch>
2014-01-29 01:29:12 +0100

    Improved version of TCP graphs.  They are still missing events

--- Dragonet/tcpGraph.hs
+++ Dragonet/tcpGraph.hs
@@ -15,14 +15,15 @@ import qualified Dragonet.Implementation as Impl
 graph tcp {
 
     node TCPQueue {
-        port out[L4TCPRxFindSocketByPacket] }
+        port out[TCPRxFindSocketByPacket] }
 
-    node EventQueue {
-        port out[L4TCPEventFindSocketByContext] }
+//    node EventQueue {
+//        port out[TCPEventFindSocketByContext] }
 
 
-    cluster L4TCP {
+    cluster TCP {
 
+/*
         cluster Event {
 
             node FindSocketByContext{
@@ -48,7 +49,7 @@ graph tcp {
             node Msl2Timer           {}
 
         } // end cluster: cluster Event
-
+*/
 
         cluster Rx {
 
@@ -56,14 +57,6 @@ graph tcp {
                 port toSocket[PacketForSocket]
                 port noSockets[ClosedPortAction]
             }
-
-/*
-            node PortClassify {
-                port p1[Sock1]
-                port p2[Sock2]
-                port noSockets[ClosedPortAction]
-            }
-*/
 
             node ClosedPortAction{
                 // drop packet
@@ -76,90 +69,205 @@ graph tcp {
             }
 
             node ServerSocket{
-                port toClose        [.StateClosedStart]
-                port toListen       [.StateListen]
-                port toSynRecv      [.StateSynSent]
-                port toEstablished  [.StateEstablished]
-                port toCloseWait    [.StateCloseWait]
-                port toLastAck      [.StateLastAck]
+                // Selects the next edge based on the current state
+                // of socket and without consulting the packet
+
+                port isListen       [IsValidSyn]
+                port isSynRecv      [IsValidAck] // for simultaneous open
+                port isEstablished  [IsDataPacket IsValidAck IsFin]
+                port isLastAck      [IsValidFinAck]
             }
 
             node ClientSocket{
-                port toClose        [.StateClosedStart]
-                port toSynSent      [.StateSynSent]
-                port toEstablished  [.StateEstablished]
-                port toFinWait1     [.StateFinWait1]
-                port toFinWait2     [.StateFinWait2]
-                port toTimeWait     [.StateTimeWait]
-                port toClosing      [.StateClosing]
+
+                port isSynSent      [IsValidSynAck]
+                port isEstablished  [IsDataPacket IsValidAck]
+                port isFinWait1     [IsValidFinAckOnly1 IsValidFinANDAck IsValidFinOnly1]
+
+                port isFinWait2     [IsValidFinOnly2]
+                port isClosing      [IsValidFinAckOnly2]
             }
 
-        } /* end cluser : RX */
 
-
-        cluster State {
-
-            node ClosedStart{
-                port toListen[Listen]
-                port toSynSent[SynSent]
+            boolean IsValidSynAck{
+                port true[.TxSendSynAck]
+                port false[]
             }
 
-            node Listen{
-                port toSynRecv[SynSent]
+
+            boolean IsValidSyn{
+                port true[.TxSendSyn]
+                port false[..DropPacket]
             }
 
-            node SynSent{
-                port toEstablished [Established]
+            boolean IsValidAck{
+                port true[.SChangeToEstb]
+                port false[..DropPacket]
             }
 
-            node SynReceived{
-                port toEstablished [Established]
+            boolean IsDataPacket{
+                port true[.TxSendAck CopyData]
+                port false[]
             }
 
-            node Established{
-                port toFinWait1 [FinWait1]
-                port toCloseWait [CloseWait]
+            boolean IsFin{
+                port true[.TxSendFinAckServer]
+                port false[]
             }
 
-            node FinWait1{
-                port toFinWait2 [FinWait2]
-                port toClosing [Closing]
-                port toTimeWait [TimeWait]
+            boolean IsValidFinAck{
+                port true[.SChangeToClosed]
+                port false[]
             }
 
-            node FinWait2{
-                port toTimeWait [TimeWait]
+            boolean IsValidFinAckOnly1{
+                // from state FinWait1
+                port true[.SChangeToFinWait2]
+                port false[]
             }
 
-            node Closing{
-                port toTimeWait [TimeWait]
+            boolean IsValidFinAckOnly2{
+                // from state closing
+                port true[.SChangeToFinWait2]
+                port false[]
             }
 
-            node TimeWait{
-                port toClosedEnd [ClosedEnd]
+
+
+            boolean IsValidFinOnly1{
+                // From FinWait1
+                port true[.TxSendFinAckClientC]
+                port false[]
+            }
+            boolean IsValidFinANDAck{
+                // From FinWait1
+                port true[.TxSendFinAckClient]
+                port false[]
             }
 
-            node CloseWait{
-                port toLastAck[LastAck]
+            boolean IsValidFinOnly2{
+                // From FinWait2
+                port true[.TxSendFinAckClient]
+                port false[]
             }
 
-            node LastAck{
-                port toClosedEnd [ClosedEnd]
+
+
+            node CopyData{
+                // Copy the data into application buffer
             }
 
-            node ClosedEnd{
-            }
+        } /* end cluster : Rx */
 
-        } // end cluster: cluster State
+
+        cluster SChange{
+
+            // These are state changes
+           node ToSynReceived {
+                port out [.ContextGeneric]
+                // Update the state to SynReceived
+           }
+
+           node ToEstb {
+                port out [.ContextGeneric]
+                // Update the state to Established
+           }
+
+           node ToCloseWait {
+                port out [.ContextGeneric]
+                // Update the state to SynReceived
+           }
+
+           node ToClosed {
+                port out [.ContextGeneric]
+                // Update the state to close
+           }
+
+            // Client state changes /////////
+           node ToClosing {
+                port out [.ContextGeneric]
+                // Update the state to closing for clients
+           }
+
+           node ToFinWait2 {
+                port out [.ContextGeneric]
+                // Update the state to FinWait2 for clients
+           }
+
+           node ToTimeWait {
+                port out [.ContextGeneric]
+                // Update the state to Timewait for clients
+           }
+
+
+        } // end cluster: SChange
+
+
+        cluster Context{
+            // These are context updates
+
+           node AckWindow{
+                // Update the ACKed packets/data
+           }
+
+           node SequenceNo{
+                // Update the next expected data
+           }
+
+           node Generic{
+                // Can Update any data about socket context
+           }
+
+
+        } // end cluster: Context
+
+
+
+        cluster Tx {
+
+           node SendSyn{
+                port out [.SChangeToSynReceived ..SendPacket]
+           }
+
+           node SendSynAck{
+                // send Ack for syn
+                port out [.SChangeToEstb ..SendPacket]
+           }
+
+
+           node SendAck{
+                port out[.ContextSequenceNo ..SendPacket]
+           }
+
+           node SendFinAckServer{
+                // send Ack for Fin (on server side)
+                port out [.SChangeToCloseWait ..SendPacket]
+           }
+
+           node SendFinAckClientC{
+                // send Ack for Fin from state FinWait1 to closing
+                port out [.SChangeToClosing ..SendPacket]
+           }
+
+           node SendFinAckClient{
+                // send Ack for Fin from state FinWait1 or FinWait2
+                port out [.SChangeToTimeWait ..SendPacket]
+           }
+
+        } // end cluster : Tx
 
 
     } // end cluster: cluster L4TCP
+
+    node SendPacket{
+        // Send the TCP packet out
+    }
+
 
 
     node DropPacket{
         // drop packet
     }
-
 
 }
 |]
