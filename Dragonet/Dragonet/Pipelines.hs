3442649ab7e178916f209d4837b09d98c866d1af
Antoine Kaufmann <antoinek@student.ethz.ch>
2014-02-20 16:28:57 +0100

    Pipeline graph structure, and function to split up protocol graph into pipelines

---
+++ Dragonet/Dragonet/Pipelines.hs
@@ -0,0 +1,108 @@
+module Dragonet.Pipelines(
+    Pipeline(..),
+    PLGraph, PLNode, PLEdge,
+    generatePLG,
+) where
+
+import qualified Control.Concurrent.STM.TChan as TC
+import qualified Data.Graph.Inductive as DGI
+import qualified Dragonet.ProtocolGraph as PG
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.List as L
+import qualified Util.GraphHelpers as GH
+import qualified Util.GraphMonad as GM
+import Data.Function
+import Data.Maybe
+import Util.Misc
+
+data Pipeline = Pipeline {
+    plLabel :: String,
+    plGraph :: PG.PGraph
+} --deriving (Show)
+
+instance Show Pipeline where
+    show pl = "Pipeline \"" ++ plLabel pl ++ "\""
+
+type PLGraph = DGI.Gr Pipeline ()
+type PLNode = DGI.LNode Pipeline
+type PLEdge = DGI.LEdge ()
+
+-- Generate one pipeline, returns list of successor pipeline labels and the
+-- pipeline
+generatePipeline :: PG.PGraph -> M.Map DGI.Node String -> String
+                        -> [DGI.Node] -> (Pipeline, [String])
+generatePipeline g nm pll ns = (Pipeline pll pg, suc)
+    where
+        -- Label a node id
+        labN n = PG.nLabel $ fromJust $ DGI.lab g n
+        -- Graph obtained by removing all nodes that don't belong to this
+        -- pipeline
+        pg' = GH.reduceNodes g ns
+        nss = S.fromList ns
+        -- Edges incoming from other pipelines
+        (inE,notInE) = L.partition
+                        (\(s,d,_) -> (S.notMember s nss) && (S.member d nss)) $
+                        DGI.labEdges g
+        inDN = L.nub $ map snd3 $ inE
+        -- Edges outgoing to other pipelines
+        (outE,_) = L.partition
+                        (\(s,d,_) -> (S.member s nss) && (S.notMember d nss)) $
+                        notInE
+        outN = partListBy snd3 outE
+        outPL = partListBy ((nm M.!) . fst) outN
+
+        -- Adapt graph
+        (suc,pg) = flip GM.runOn pg' (do
+            -- Demux node for incoming edges
+            let demuxPs = map labN inDN ++ ["_"]
+            demuxN <- GM.newNode $ PG.baseFNode "Demux" [] demuxPs Nothing
+            mapM_ (\n -> GM.newEdge (demuxN,n, labN n)) inDN
+            -- TODO: Handle original source nodes
+
+            -- Destination nodes for outgoing queues
+            if null outE then
+                return ()
+            else
+                mapM_ (\(pl,ns') -> do
+                    -- Create "To$Pipeline" node
+                    toPLN <- GM.newNode $
+                        PG.baseFNode ("ToPL" ++ pl) [] ["out"] Nothing
+                    mapM_ (\(n,es) -> do
+                        -- Create "To$Node" node
+                        toNN <- GM.newNode $
+                            PG.baseFNode ("ToPL" ++ pl ++ "_" ++ labN n) []
+                                ["out"] Nothing
+                        -- Add edge to "To$Pipeline" node
+                        GM.newEdge (toNN,toPLN,"out")
+                        -- Add edges
+                        mapM_ (\(s,_,p) -> GM.newEdge (s,toNN,p)) es
+                        ) ns'
+                    ) outPL
+            return $ map fst outPL)
+
+
+-- Splits specified graph up into pipelines according to the specified function,
+-- that assigns a string representing the pipeline label to each node
+generatePLG :: (PG.PGNode -> String) -> PG.PGraph -> PLGraph
+generatePLG sf g = DGI.mkGraph nodes edges
+    where
+        -- Group nodes by pipeline
+        plns' = partListBy sf $ DGI.labNodes g
+        -- Map from PG node id to pipeline label
+        nmp = M.fromList $ map (\ln@(n,_) -> (n, sf ln)) $ DGI.labNodes g
+
+        -- Assignment from pipeline label to node id in PLG
+        plids = M.fromList $ flip L.zip [0..] $ map fst plns'
+        -- Create pipelines
+        pipelines = map (\(pll,ns) ->
+            (plids M.! pll,
+             generatePipeline g nmp pll $ map fst ns)
+            ) plns'
+
+        -- Labeled nodes for pipeline graph
+        nodes = map (\(n,(p,_)) -> (n,p)) pipelines
+        -- Edges for pipeline graph
+        edges = concatMap (\(n,(_,suc)) -> map (\x -> (n,plids M.! x,())) suc)
+                    pipelines
+
--- Dragonet/Util/Misc.hs
+++ Dragonet/Util/Misc.hs
@@ -1,9 +1,11 @@
 module Util.Misc (
-    splitBy, joinBy, minusL,
+    splitBy, joinBy, minusL, partListBy,
     fst3, snd3, thd3,
 ) where
 
 import qualified Data.List as L
+import Data.Function
+import qualified Data.Map as M
 
 -- Split list by delimiter into multiple lists
 splitBy :: Eq a => a -> [a] -> [[a]]
@@ -21,6 +23,16 @@ joinBy l sep = L.foldl1 (\a b -> a ++ sep ++ b) l
 -- Setminus on unordered lists (inefficient)
 minusL :: (Eq a) => [a] -> [a] -> [a]
 minusL a b = filter (not . (flip elem b)) a
+
+-- Partition a list into multiple labeled sublists according to function
+partListBy :: (Ord b) => (a -> b) -> [a] -> [(b, [a])]
+partListBy f xs = M.toList $ foldl
+    (\m x ->
+        let k = f x
+            l = if M.member k m then m M.! k else []
+        in
+            M.insert k (x:l) m) M.empty xs
+
 
 fst3 :: (a,b,c) -> a
 fst3 (a,_,_) = a
