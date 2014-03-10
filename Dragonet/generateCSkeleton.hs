8e741b3bc71ef77503ca4be4f56f2f2539582068
Antoine Kaufmann <antoinek@student.ethz.ch>
2014-03-10 18:04:45 +0100

    Also generate basic C function to run on a packet for testing purposes

--- Dragonet/generateCSkeleton.hs
+++ Dragonet/generateCSkeleton.hs
@@ -1,5 +1,8 @@
+import Dragonet.ProtocolGraph
 import qualified Dragonet.Unicorn.Parser as UP
 import qualified Dragonet.Unicorn as Unicorn
+import qualified Data.Graph.Inductive as DGI
+import qualified Util.GraphHelpers as GH
 import Data.Function
 import Data.Maybe
 import qualified Data.List as L
@@ -56,6 +59,60 @@ generateFNSkels g = L.intercalate "\n\n" $ mapMaybe proto $ graphNodes g
             return (sig ++ body)
 
 
+
+nodeStmts :: PGraph -> PGNode -> [String]
+nodeStmts g (n,l) =
+    ["if (" ++ guardE ++ ") {", "    " ++ nLabel l ++ " = " ++ callS, dbg, "}"]
+    where
+        isBoolean l' = (elem "Boolean" $ nAttributes l') || nIsONode l'
+        pLabel l' p =
+            if isBoolean l'
+                then ("P_" ++ p)
+                else ("P_" ++ nLabel l' ++ "_" ++ p)
+        nfLabel l' = "do_pg__" ++ nLabel l'
+        guardE
+            | nIsFNode l = if null pre
+                then "1"
+                else L.intercalate " || " $
+                    map (\(n',p) -> (nLabel n' ++ " == " ++ pLabel n' p)) pre
+            | nIsONode l = L.intercalate " && " $
+                map (\(n',p) -> (nLabel n' ++ " != -1")) pre
+            where
+                pre = map (\(n',e) -> (fromJust $ DGI.lab g n',e)) $
+                    DGI.lpre g n
+        callS
+            | nIsFNode l = nfLabel l ++ "(st, in);"
+            | nIsONode l = "(" ++ onCond ++ " ? P_true : P_false);"
+        onCond =
+            case op of
+                OpAnd -> L.intercalate " && " $
+                    map (\l' -> nLabel l' ++ " == P_true") inT
+                OpOr -> L.intercalate " || " $
+                    map (\l' -> nLabel l' ++ " == P_true") inT
+           where
+                (ONode op) = nPersonality l
+                inT = map (fromJust . DGI.lab g . fst) $
+                    filter (\(_,e) -> e == "true") $ DGI.lpre g n
+                inF = map (fromJust . DGI.lab g . fst) $
+                    filter (\(_,e) -> e == "false") $ DGI.lpre g n
+        dbg = "    printf(\"" ++ nLabel l ++ "=%d\\n\", " ++ nLabel l ++ ");"
+
+generateTestFun :: String -> PGraph -> String
+generateTestFun name g = sig ++ "\n{\n" ++ body ++ "\n}\n"
+    where
+        stP = "st"
+        inP = "in"
+        sig = "void " ++ name ++ "(struct state * " ++ stP ++
+            ", struct input *" ++ inP ++ ")"
+        body = L.intercalate "\n" $ map ("    " ++) bodyL
+        bodyL = [nvDecl] ++ stmts
+        nvDecl = "node_out_t " ++
+            (L.intercalate ", " $ map (\n -> nLabel n ++ "=-1") $ map snd $
+                DGI.labNodes g) ++ ";"
+        stmts = concatMap (nodeStmts g) $ GH.topsortLN g
+
+
+
 main :: IO ()
 main = do
     txt <- readFile "lpgIcmpImpl.unicorn"
@@ -65,7 +122,8 @@ main = do
     putStrLn $ generateFNProtos igraph
     putStrLn "\n"
     putStrLn $ generateFNSkels igraph
-    --putStrLn $ generatePortEnum "node_ports" igraph
-    --let graph = Unicorn.constructGraph igraph
+    putStrLn "\n"
+    let graph = Unicorn.constructGraph igraph
+    putStrLn $ generateTestFun "testFun" graph
     return ()
 
