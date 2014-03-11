b28f71524cc3ba116ba1ba72567b2448bf15432e
Antoine Kaufmann <antoinek@student.ethz.ch>
2014-03-11 15:17:12 +0100

    Filled in skeletons for functions reading packet contents

--- Dragonet/c_impl/include/packet_access.h
+++ Dragonet/c_impl/include/packet_access.h
@@ -6,13 +6,13 @@
 #include <string.h>
 #include <implementation.h>
 
+#define CHECK_BOUNDS(in,o,l,f) do { if (o + l > in->len) \
+    panic("Out of bounds in "#f"\n") } while (0)
+
 /** Read `len' bytes from offset `off' to buffer `d' */
 static inline void pkt_read(struct input *in, size_t off, size_t len, void *d)
 {
-    if (off + len > in->len) {
-        panic("Out of bounds in pkt_read\n");
-    }
-
+    CHECK_BOUNDS(in, off, len, "pkt_read");
     memcpy(d, (uint8_t *) in->data + off, len);
 }
 
@@ -21,27 +21,35 @@ static inline void pkt_read(struct input *in, size_t off, size_t len, void *d)
 
 static inline uint8_t pkt_read8(struct input *in, size_t off)
 {
-    return 0;
+    CHECK_BOUNDS(in, off, 1, "pkt_read8");
+    return *((uint8_t *) in->data + off);
 }
 
 static inline uint16_t pkt_read16(struct input *in, size_t off)
 {
-    return 0;
+    CHECK_BOUNDS(in, off, 2, "pkt_read16");
+    return *((uint16_t *) ((uint8_t *) in->data + off));
 }
 
 static inline uint32_t pkt_read32(struct input *in, size_t off)
 {
-    return 0;
+    CHECK_BOUNDS(in, off, 4, "pkt_read32");
+    return *((uint32_t *) ((uint8_t *) in->data + off));
 }
 
 static inline uint64_t pkt_read48(struct input *in, size_t off)
 {
-    return 0;
+    CHECK_BOUNDS(in, off, 6, "pkt_read48");
+    // TODO: We can probably do this faster
+    uint64_t v = 0;
+    pkt_read(in, off, 6, &v);
+    return v;
 }
 
 static inline uint64_t pkt_read64(struct input *in, size_t off)
 {
-    return 0;
+    CHECK_BOUNDS(in, off, 8, "pkt_read64");
+    return *((uint64_t *) ((uint8_t *) in->data + off));
 }
 
 
@@ -50,22 +58,24 @@ static inline uint64_t pkt_read64(struct input *in, size_t off)
 
 static inline uint16_t pkt_read16be(struct input *in, size_t off)
 {
-    return 0;
+    uint16_t v = pkt_read16(in, off);
+    // __builtin_bswap16 is missing currently (gcc #52624)
+    return (v << 8) | (v >> 8);
 }
 
 static inline uint32_t pkt_read32be(struct input *in, size_t off)
 {
-    return 0;
+    return __builtin_bswap32(pkt_read32(in, off));
 }
 
 static inline uint64_t pkt_read48be(struct input *in, size_t off)
 {
-    return 0;
+    return __builtin_bswap64(pkt_read64(in, off) << 16);
 }
 
 static inline uint64_t pkt_read64be(struct input *in, size_t off)
 {
-    return 0;
+    return __builtin_bswap64(pkt_read64(in, off));
 }
 
 
@@ -74,22 +84,22 @@ static inline uint64_t pkt_read64be(struct input *in, size_t off)
 
 static inline uint16_t pkt_read16le(struct input *in, size_t off)
 {
-    return 0;
+    return pkt_read16(in, off);
 }
 
 static inline uint32_t pkt_read32le(struct input *in, size_t off)
 {
-    return 0;
+    return pkt_read32(in, off);
 }
 
 static inline uint64_t pkt_read48le(struct input *in, size_t off)
 {
-    return 0;
+    return pkt_read48(in, off);
 }
 
 static inline uint64_t pkt_read64le(struct input *in, size_t off)
 {
-    return 0;
+    return pkt_read64(in, off);
 }
 
 
