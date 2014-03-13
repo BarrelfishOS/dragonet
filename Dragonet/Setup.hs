811f0137b1256d2d251816e4ee66153700ebcd7a
Antoine Kaufmann <antoinek@student.ethz.ch>
2014-03-13 14:33:24 +0100

    Build llvm-cgen also with cabal

--- Dragonet/Setup.hs
+++ Dragonet/Setup.hs
@@ -1,2 +1,48 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Program
+import Distribution.Verbosity
+import qualified System.Directory as SD
+import System.FilePath
+
+main = defaultMainWithHooks simpleUserHooks {
+    buildHook = myBuildHook,
+    hookedPrograms = hookedPrograms simpleUserHooks ++
+        [simpleProgram "clang",
+         simpleProgram "llvm-link"]
+    }
+
+llvmSources = [ "c_impl/impl_arp.c", "c_impl/impl_ethernet.c",
+    "c_impl/impl_icmp.c", "c_impl/impl_ipv4.c", "c_impl/impl_udp.c",
+    "c_impl/impl_tcp.c", "c_impl/impl_misc.c", "c_impl/implementation.c",
+    "c_impl/support/Ethernet.c", "c_impl/support/Icmp.c", "lib/Util/tap.c" ]
+
+-- Run clang to get llvm bitcode
+llvmClang prog opts incls outD src = do
+    let outD' = outD </> dropFileName src
+        out_file = outD' </> replaceExtension (takeFileName src) ".bc"
+        iopts = map ("-I" ++) incls
+        opts' = opts ++ iopts ++ ["-emit-llvm", "-c", src, "-o", out_file]
+    SD.createDirectoryIfMissing True outD'
+    runProgram verbose prog opts'
+    return out_file
+
+-- Link llvm bitcode files together
+llvmLink prog opts outD outF srcs = do
+    let outP = outD </> outF
+        opts = srcs ++ ["-o", outP]
+    runProgram verbose prog opts
+    return outP
+
+
+myBuildHook pkg_descr local_bld_info user_hooks bld_flags = do
+    let bld_dir   = buildDir local_bld_info
+        progs = withPrograms local_bld_info
+        (Just clang) = lookupProgram (simpleProgram "clang") progs
+        (Just llvmlink) = lookupProgram (simpleProgram "llvm-link") progs
+    -- Build llvm bitcode for C implementation of graph
+    llvmBCs <- mapM (llvmClang clang [] ["c_impl/include"] bld_dir) llvmSources
+    -- Combine into one bitcode file
+    llvmLink llvmlink [] bld_dir "llvm-helpers.bc" llvmBCs
+    -- Default build hook
+    buildHook simpleUserHooks pkg_descr local_bld_info user_hooks bld_flags
