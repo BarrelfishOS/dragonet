3a60ae6f5c21334603e7fc1a27a8cd7c10857a99
Antoine Kaufmann <antoinek@student.ethz.ch>
2014-03-13 02:14:36 +0100

    Avoid copying payload around in ICMP

--- Dragonet/c_impl/impl_icmp.c
+++ Dragonet/c_impl/impl_icmp.c
@@ -36,30 +36,18 @@ node_out_t do_pg__TxL3ICMPInitiateResponse(struct state *state, struct input *in
 {
     // get packet fields to be sent out.
     uint32_t id = icmp_hdr_misc_read(in);
-//    pktoff_t payload_off = icmp_payload_offset(in);
-    pktoff_t payload_len = icmp_payload_length(in);
-
     uint32_t srcIP = ipv4_srcIP_rd(in);
     uint32_t dstIP = ipv4_dstIP_rd(in);
-    uint8_t payload[DEFAULT_BUFFER_SIZE];
 
-    // copy out the payload
-    int ret = icmp_copy_payload(in, payload, sizeof(payload));
-    if (ret < 0) {
-        return P_TxL3ICMPInitiateResponse_drop;
-    }
-
-    // clean up the packet and attributes
-    input_clean_packet(in);
+    // Throw out all headers up to and including ICMP
+    pkt_prepend(in, - (ssize_t) icmp_payload_offset(in));
+    // clean up the attributes
     input_clean_attrs(in);
 
     in->ip4_dst = srcIP;
     in->ip4_src = dstIP;  // FIXME: maybe I should read it from global state
     in->icmp_id = id;
 
-    // copy the payload to packet
-    pkt_prepend(in, payload_len);
-    pkt_write(in, 0, payload_len, payload);
     return P_TxL3ICMPInitiateResponse_out;
 }
 
