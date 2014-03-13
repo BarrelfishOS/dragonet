d2136db45bc98507e184495d46df2350cb8b13a3
Pravin Shinde <pravin@inf.ethz.ch>
2014-03-13 03:22:08 +0100

    Fixed minor error of mistyped variable name

--- Dragonet/c_impl/impl_misc.c
+++ Dragonet/c_impl/impl_misc.c
@@ -58,18 +58,21 @@ node_out_t do_pg__TxQueue(struct state *state, struct input *in)
     dprint("Send!: ");
     int ret = 0;
     // Sending out packet with tuntap
-    if ( state->drv_state != NULL &&
-            state->drv_state->drv_handle != NULL &&
-            state->drv_state->drv_tx != NULL) {
-
-            ret = state->drv_state->drv_tx(state->drv_state->drv_handle,
-                    in->data, in->len);
-            if (ret < 0) {
-                panic("packet sending failed\n");
-            }
-        //tap_write(tap_dn, in->data, in->len);
-    }
     //input_dump(in);
+
+    // Removing following sanity checks as they are done during initialization
+/*    if ( state->driver_handler == NULL ||
+            state->driver_handler->drv_handle == NULL ||
+            state->driver_handler->drv_tx == NULL) {
+            printf("Error: Device driver not configured\n");
+            return 0;
+    }
+*/
+    ret = state->driver_handler->drv_tx(state->driver_handler->drv_handle,
+                    in->data, in->len);
+    if (ret < 0) {
+        panic("packet sending failed\n");
+    }
     return 0;
 }
 
