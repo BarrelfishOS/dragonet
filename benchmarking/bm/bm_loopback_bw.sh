2f9028b13dd64f15e352d96bfe521a7bade18b69
Pravin Shinde <pravin@inf.ethz.ch>
2014-03-27 09:47:42 +0100

    try to get benchmarks working in new setup

---
+++ benchmarking/bm/bm_loopback_bw.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -x
+set -e
+
+
+runServer() {
+    taskset -c  ${CORE_SERVER}  ${LOADSINK} -4 -L ${SERVERIP}
+    sleep 2
+}
+
+recordServer() {
+    ${DSTAT} -tcny --net-packets -N ${IFACE_SERVER} -C ${CORE_SERVER} --bits --output "${SERVER_DSTAT}.csv" > ${SERVER_DSTAT}.dstat &
+    DSTAT_SERVER_PID=$!
+}
+
+recordClient() {
+    ${DSTAT} -tcny --net-packets -N ${IFACE_CLIENT} -C ${CORE_CLIENT} --bits --output "${CLIENT_DSTAT}.csv" > ${CLIENT_DSTAT}.dstat &
+    DSTAT_CLIENT_PID=$!
+}
+
+stopRecording() {
+    kill ${DSTAT_SERVER_PID}
+    kill ${DSTAT_CLIENT_PID}
+}
+
+killServer() {
+    local toKill=`basename ${LOADSINK}`
+    echo "killing ${toKill}"
+    killall ${toKill}
+}
+
+
+MACHINE="127.0.0.1"
+SERVERIP="127.0.0.1"
+CORE_SERVER=3
+CORE_CLIENT=1
+IFACE_SERVER="lo"
+IFACE_CLIENT="lo"
+
+DSTAT="../dstat/dstat"
+NPERF_DIR="../dstat/dstat"
+
+OUTPUTDIR="../output/"
+
+LOADSINK="../netperf-2.6.0/src/netserver"
+
+SERVERPIP="127.0.0.1"
+
+NPW="../netperf-wrapper/netperf-wrapper"
+LOADGEN="../netperf-2.6.0/src/netperf"
+
+
+mkdir -p  ${OUTPUTDIR}
+
+SERVER_DSTAT=`mktemp --tmpdir=${OUTPUTDIR} dserver_XXXXXX`
+CLIENT_DSTAT=`mktemp --tmpdir=${OUTPUTDIR} dclient_XXXXXX`
+
+
+# kill any previous instances
+# killall python || true
+
+killall dstat || true
+killServer || true
+runServer
+recordServer
+recordClient
+taskset -c ${CORE_CLIENT} ${LOADGEN} -P 0 -l 5 -t UDP_STREAM -fg -H ${SERVERIP} -- -r 8900 -k all
+stopRecording || true
+killServer || true
+echo "Server Logs: ${SERVER_DSTAT}"
+echo "Client Logs: ${CLIENT_DSTAT}"
+echo "Done..."
+
---
+++ benchmarking/netperf-2.6.0/configure_compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x
+set -e
+./configure --enable-demo
+make -j1
+
+
