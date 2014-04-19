46bfcdb0498bd46c9191dfb5b322ef51120e00e8
Antoine Kaufmann <antoinek@student.ethz.ch>
2014-04-19 19:45:07 +0200

    Clean up Dragonet directory by moving scripts out

--- Dragonet/test_scripts/nc_test_sf.sh
+++ Dragonet/test_scripts/nc_test_sf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+nc.traditional -n -vv -s 10.113.4.38 -u -p 5555 10.113.4.71 5556
 
-#nc.traditional -n -vv -s 10.113.4.38 -u -p 5555  10.113.4.71 51098
-nc.traditional -n -vv -s 10.113.4.38 -u -p 5555  10.113.4.71 7
+# ping -I eth3 10.113.4.71
+# # to find the proper interface
+# ifconfig -a | grep "00:1b:21:8f:18:65"
 
