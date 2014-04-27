5cda68543ffdd44b2bd45d2197140040e41eaca3
Pravin Shinde <pravin@inf.ethz.ch>
2014-04-27 21:25:03 +0200

    script to automate configuration and installation of memaslap

---
+++ benchmarking/libmemcached-1.0.18/configure_install_dragonet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+set -e
+LIBS=-lpthread ./configure --enable-memaslap
+make
+sudo make install
+sudo cp ./libmemcached/.libs/libmemcached.so.11.0.0 /usr/lib/x86_64-linux-gnu/libmemcached.so.11
+
