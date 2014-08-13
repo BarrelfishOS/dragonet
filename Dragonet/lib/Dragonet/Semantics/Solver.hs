39a564c6cbb65d9d9a190aa69816e574e7028cb6
Antoine Kaufmann <toni@famkaufmann.info>
2014-08-13 21:41:55 +0200

    Fix typo in Semantics.Solver error message

--- Dragonet/lib/Dragonet/Semantics/Solver.hs
+++ Dragonet/lib/Dragonet/Semantics/Solver.hs
@@ -31,14 +31,14 @@ class (Monad m, MonadIO m) => SolverM m e | m -> e where
     exprFalse :: m e
 
 exprAndL :: SolverM m e => [e] -> m e
-exprAndL [] = error "exprAndL applied to non-empty list"
+exprAndL [] = error "exprAndL applied to empty list"
 exprAndL [e] = return e
 exprAndL (e:es) = do
     e' <- exprAndL es
     exprAnd e e'
 
 exprOrL :: SolverM m e => [e] -> m e
-exprOrL [] = error "exprAndL applied to non-empty list"
+exprOrL [] = error "exprAndL applied to empty list"
 exprOrL [e] = return e
 exprOrL (e:es) = do
     e' <- exprOrL es
