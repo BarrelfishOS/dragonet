846c212982e1fc4c09f129875f52c8daa0b288af
Pravin <pravin@inf.ethz.ch>
2014-09-24 15:50:35 +0200

    removed incorrect results, and some script improvments.

--- benchmarking/netperf-wrapper/manual_data_collection_SF_memcached.sh
+++ benchmarking/netperf-wrapper/manual_data_collection_SF_memcached.sh
@@ -4,7 +4,7 @@ startStack() {
 ./cleanupServer.sh
 ./netperf-wrapper -d 0 --udp --serverCoreShift 0  ${ClientList} --servercores ${SRVCORES} \
 --serverInstances 1 --hwqueues ${HWQUEUE} --clientcores 1   --packet ${PACKETSIZE} --concurrency 1 \
--I 1 -l 5 -H asiago -T 10.113.4.95 -c ${ECHO_SERVER} ${UDP_TEST_NAME} --totalClients ${CLIENTCOUNT}
+-I 1 -l 5 -H asiago -T ${SERVERIP} -c ${ECHO_SERVER} ${UDP_TEST_NAME} --totalClients ${CLIENTCOUNT}
 
 exit 0
 }
@@ -12,7 +12,7 @@ exit 0
 check_working_dummy() {
 ./netperf-wrapper -d 0 --udp --serverCoreShift 0  ${ClientList} --servercores ${SRVCORES} \
 --serverInstances 1 --hwqueues ${HWQUEUE} --clientcores 1 --packet ${PACKETSIZE} --concurrency ${CONCURRENTY} \
--I 1 -l 15 -H asiago -T 10.113.4.95 -c noServer ${UDP_TEST_NAME} --totalClients ${CLIENTCOUNT}
+-I 1 -l 15 -H asiago -T ${SERVERIP} -c noServer ${UDP_TEST_NAME} --totalClients ${CLIENTCOUNT}
 exit 0
 }
 
@@ -20,38 +20,45 @@ exit 0
 
 getData() {
 
-title="Priority_${ECHO_SERVER},CLC_${CLIENTCOUNT},PKT_${PACKETSIZE}"
+title="Test_${UDP_TEST_NAME},CONCUR_${CONCURRENTY},PKT_${PACKETSIZE},${NICTYPE},SRV_${ECHO_SERVER},CLC_${CLIENTCOUNT}"
 
-OUTDIR="./nsdi_data/priority_v3/Test_${UDP_TEST_NAME}/PKT_${PACKETSIZE}/NIC_Intel/SRV_${ECHO_SERVER}/"
+OUTDIR="./nsdi_data/Test_${UDP_TEST_NAME}/CONCUR_${CONCURRENTY}/PKT_${PACKETSIZE}/${NICTYPE}/SRV_${ECHO_SERVER}/"
 mkdir -p "${OUTDIR}"
 
 ./netperf-wrapper -d 0 --udp --serverCoreShift 0  ${ClientList} --servercores ${SRVCORES} \
 --serverInstances 1 --hwqueues ${HWQUEUE} --clientcores 1 --packet ${PACKETSIZE} --concurrency ${CONCURRENTY} \
--I 3 -l 50 -H asiago -T 10.113.4.95 -c noServer ${UDP_TEST_NAME} --totalClients ${CLIENTCOUNT} -t ${title} -o ${OUTDIR}
+-I 3 -l 50 -H asiago -T ${SERVERIP} -c noServer ${UDP_TEST_NAME} --totalClients ${CLIENTCOUNT} \
+-t ${title} -o ${OUTDIR} -L  "${OUTDIR}/${title}.runlog"
 }
 
 ClientList="-C ziger1 -C ziger2 -C burrata -C gottardo -C appenzeller-e1000 -C sbrinz1 -C gruyere -C sbrinz2 "
 
-ECHO_SERVER="llvmE10k"
+
+SERVERIP="10.113.4.195"
+NICTYPE="NIC_SF"
+ECHO_SERVER="llvmSF"
 UDP_TEST_NAME="memcached_rr"
 HWQUEUE=10
 CONCURRENTY=1
 PACKETSIZE=1024
-
 CLIENTCOUNT=8
 SRVCORES=8
 
 
-SRVCORES=10
 CLIENTCOUNT=32
-
 CLIENTCOUNT=24
+CLIENTCOUNT=16
 
-CLIENTCOUNT=40
+
+
 set -x
 set -e
+SRVCORES=10
 
-#startStack
+CLIENTCOUNT=40
+
+startStack
+
 #check_working_dummy
 
 PACKETSIZE=64
